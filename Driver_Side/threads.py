2a91c7d2e581d416fc57ea6585f7af7b1b7e2c18
Holiday <hpettijohn20@vcschools.org>
2019-02-08 21:22:09 -0800

    Update threads.py

--- Driver_Side/threads.py
+++ Driver_Side/threads.py
@@ -4,6 +4,10 @@
 import comsock
 import threading
 
+import menus
+
+matchfunctions = menus.matchfunctions
+
 class SocketThread(threading.Thread):
   def __init__(self, threadID, name, counter, timeout = 180):
     threading.Thread.__init__(self)
@@ -15,3 +19,17 @@ class SocketThread(threading.Thread):
   def run(self):
     self.daemon = True
     comsock.mainloop(self.timeout)
+
+class SystemThread(threading.Thread):
+  def __init__(self, threadID=0, name="", counter=0, roundtime = 180):
+    threading.Thread.__init__(self)
+    self.threadID = threadID
+    self.name = name
+    self.counter = counter
+    self.roundtime = roundtime
+    self.lastinterface = "mainmenu"
+
+  def run(self, win):
+    if win.interface != self.lastinterface:
+      matchfunctions[win.interface](win)
+      self.lastinterface = win.interface
