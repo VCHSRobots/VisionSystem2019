d6bb0c3135d786756525f5eaf7b1d4516e84ebc9
Holiday <hpettijohn20@vcschools.org>
2019-02-13 17:06:45 -0800

    Added manufacturer checking to stdreader

--- Pi_Side/stdreader.py
+++ Pi_Side/stdreader.py
@@ -20,21 +20,42 @@ def getStdOutput(script):
   script = openBashScript(script)
   process = sub.Popen(script, shell=True, executable="/bin/bash", stdout=sub.PIPE)
   #Gives process time to print
-  time.sleep(.1)
+  time.sleep(.05)
   output = process.stdout.read()
   return output
 
 def scanForCameras():
-  output = getStdOutput(script="getusbs").decode()
+  tree = getStdOutput(script="getusbs").decode()
+  manufacts = getStdOutput(script="getmans").decode()
   cams = []
   for camnum in range(4):
-    if scanForCam(camnum, output):
+    if scanForCam(camnum, tree, manufacts):
       cams.append(camnum)
   return cams
 
-def scanForCam(cam, lsout):
-  lsout = lsout.splitlines()
+def scanForCam(cam, tree, manufacts):
+  tree = tree.splitlines()
   foundind = -1
+  #Device number
+  dnum = findDeviceInCamPort(cam, tree)
+  if dnum == -1:
+    return False
+  manufact = getDeviceManufacturer(dnum, manufacts)
+  if manufact == "045e":
+    return True
+  else:
+    return False
+
+def getDeviceManufacturer(dnum, manufacts):
+  deviceid = "Device {:03}".format(dnum)
+  for line in manufacts.splitlines:
+    if deviceid in line:
+      manufact = line.split()[5][:4]
+      break
+  return manufact
+
+def findDeviceInCamPort(cam, tree);
+  #Checks for a device in the appropriate port
   if cam == 0:
     initsearch = "{} Port 2".format(getPrefix(indents=3))
   elif cam == 1:
@@ -43,18 +64,13 @@ def scanForCam(cam, lsout):
     initsearch = "{} Port 3".format(getPrefix(indents=2))
   elif cam == 3:
     initsearch = "{} Port 2".format(getPrefix(indents=2))
-  for ind, line in enumerate(lsout):
+  for ind, line in enumerate(tree):
+    #Checks if a device is plugged into the port
     if initsearch == line[:len(initsearch)]:
-      foundind = ind
-      break
-  if foundind == -1:
-    return False
-  for line in lsout[foundind:foundind+4]:
-    if not initsearch in line:
-      return False
-    if (not "Class=Video" in line and not "Class=Audio" in line) or (not "480M" in line):
-      return False
-  return True
+      foundind = True
+      #Checks if the device is a camera with proper specs
+      if (not "Class=Video" in line and not "Class=Audio" in line) or (not "480M" in line):
+        return False
 
 def getPrefix(indents):
   """
