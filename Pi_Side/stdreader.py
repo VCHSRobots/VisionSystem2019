e2c3757d2d76cc03e952970c6dfc955445341ca1
Holiday <hpettijohn20@vcschools.org>
2019-02-13 19:00:40 -0800

    Prepared system for test

--- Pi_Side/stdreader.py
+++ Pi_Side/stdreader.py
@@ -20,31 +20,9 @@ def getStdOutput(script):
   script = openBashScript(script)
   process = sub.Popen(script, shell=True, executable="/bin/bash", stdout=sub.PIPE)
   #Gives process time to print
-  time.sleep(.05)
+  time.sleep(.025)
   output = process.stdout.read()
   return output
-
-ttree = """/:  Bus 01.Port 1: Dev 1, Class=root_hub, Driver=dwc_otg/1p, 480M
-    |__ Port 1: Dev 2, If 0, Class=Hub, Driver=hub/4p, 480M
-        |__ Port 1: Dev 3, If 0, Class=Hub, Driver=hub/3p, 480M
-            |__ Port 1: Dev 7, If 0, Class=Vendor Specific Class, Driver=lan78xx, 480M
-            |__ Port 2: Dev 5, If 0, Class=Video, Driver=uvcvideo, 480M
-            |__ Port 2: Dev 5, If 1, Class=Video, Driver=uvcvideo, 480M
-            |__ Port 2: Dev 5, If 2, Class=Audio, Driver=snd-usb-audio, 480M
-            |__ Port 2: Dev 5, If 3, Class=Audio, Driver=snd-usb-audio, 480M
-            |__ Port 3: Dev 6, If 0, Class=Video, Driver=uvcvideo, 480M
-            |__ Port 3: Dev 6, If 3, Class=Audio, Driver=snd-usb-audio, 480M
-            |__ Port 3: Dev 6, If 1, Class=Video, Driver=uvcvideo, 480M
-            |__ Port 3: Dev 6, If 2, Class=Audio, Driver=snd-usb-audio, 480M
-        |__ Port 2: Dev 4, If 0, Class=Human Interface Device, Driver=usbhid, 1.5M"""
-
-tmanus = """Bus 001 Device 004: ID 046d:c05a Logitech, Inc. M90/M100 Optical Mouse
-Bus 001 Device 006: ID 045e:0779 Microsoft Corp. LifeCam HD-3000
-Bus 001 Device 005: ID 045e:0779 Microsoft Corp. LifeCam HD-3000
-Bus 001 Device 007: ID 0424:7800 Standard Microsystems Corp.
-Bus 001 Device 003: ID 0424:2514 Standard Microsystems Corp. USB 2.0 Hub
-Bus 001 Device 002: ID 0424:2514 Standard Microsystems Corp. USB 2.0 Hub
-Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub"""
 
 def scanForCameras():
   tree = getStdOutput(script="getusbs").decode()
